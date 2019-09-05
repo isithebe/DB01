130be424246771736e83db3943f76f64bb87892d
isithebe <51477014+isithebe@users.noreply.github.com>
2019-09-05 12:06:10 +0200

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,10 @@
 # This is a template for a Ruby scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
+require 'scraperwiki'
+require 'mechanize'
 #
-# agent = Mechanize.new
+agent = Mechanize.new
 #
 # # Read in a page
 page = agent.get("https://www.a1tourism.com/uk/guernsey.html")
